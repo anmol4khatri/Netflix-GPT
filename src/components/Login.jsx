c9a4922c78cfed7b685a28afa085f33a08a550a3
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-05-12 23:57:10 +0530

    feat: implement form validation for login with email and password checks

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,22 @@
 import { Netflix_Logo } from "../utils/constants";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import {checkValidData} from "../utils/loginValidation"
 
 const Login = () => {
 
+    //Switch btw SignIn/SignUp
     const [IsSignInPage, setIsSignInPage] = useState(true);
     const handleToggle = () => {
         setIsSignInPage(!IsSignInPage);
+    };
+
+    //Form Validation
+    const email = useRef();
+    const password = useRef();
+    const[ErrorMessage,setErrorMessage] = useState(null);
+    const handleSubmit = () => {
+        const message = checkValidData(email.current.value,password.current.value);
+        setErrorMessage(message);
     };
 
     return (
@@ -29,15 +40,16 @@ const Login = () => {
             <div className="absolute w-4/12 h-8/12 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black/85 p-14
       rounded-lg mt-8 max-sm:bg-black max-sm:w-full max-sm:h-full max-sm:mt-0 max-sm:rounded-none max-sm:p-7">
                 <h3 className="text-white text-3xl font-bold max-sm:pt-12">{IsSignInPage ? "Sign In" : "Sign Up"}</h3>
-                <form action="" className="flex flex-col pt-8 gap-4">
+                <form onSubmit={(e) => e.preventDefault()} action="" className="flex flex-col pt-8 gap-4">
+                    <p className="text-red-400 font-bold text-lg">{ErrorMessage}</p>
                     {!IsSignInPage &&
                         <div className="flex gap-3">
                             <input type="text" placeholder="First Name" className="text-lg text-white border-[0.1px] rounded-lg p-3 w-1/2 " />
                             <input type="text" placeholder="Last Name" className="text-lg text-white border-[0.1px] rounded-lg p-3 w-1/2" />
                         </div>}
-                    <input type="text" placeholder="Email or mobile number" className="text-lg text-white border-[0.1px] rounded-lg p-3" />
-                    <input type="password" placeholder={IsSignInPage ? "Password" : "Create Password"} className="text-lg text-white border-[0.1px] rounded-lg p-3 " />
-                    <button className="text-white font-medium bg-red-600 rounded-lg p-3 cursor-pointer">{IsSignInPage ? "Sign In" : "Sign Up"}</button>
+                    <input ref={email} type="text" placeholder="Email or mobile number" className="text-lg text-white border-[0.1px] rounded-lg p-3" />
+                    <input ref={password} type="password" placeholder={IsSignInPage ? "Password" : "Create Password"} className="text-lg text-white border-[0.1px] rounded-lg p-3 " />
+                    <button onClick={handleSubmit} className="text-white font-medium bg-red-600 rounded-lg p-3 cursor-pointer">{IsSignInPage ? "Sign In" : "Sign Up"}</button>
                     <div className="space-x-2 my-4">
                         <span className="text-gray-300">{IsSignInPage ? "New to Netflix ?" : "Already have a account ?"}</span>
                         <span className="text-white font-semibold cursor-pointer" onClick={handleToggle}>{IsSignInPage ? "Sign up now" : "Sign in"}</span>
