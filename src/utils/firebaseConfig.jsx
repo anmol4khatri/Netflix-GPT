0606036015c48f0eb369367e87c8a26706eab8b0
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-05-13 22:41:54 +0530

    feat: add Firebase configuration and setup hosting files
    Add Firebase hosting and configuration setup
    
    Sets up Firebase hosting with deployment configuration, including hosting rewrites and caching files.
    Implements Firebase initialization and analytics integration with a basic 404 page and index page for hosting.

---
+++ src/utils/firebaseConfig.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDRTrjwcmpVeQt5jNTEYF98LRSFmfPPhqE",
+  authDomain: "netflix-gpt-0404.firebaseapp.com",
+  projectId: "netflix-gpt-0404",
+  storageBucket: "netflix-gpt-0404.firebasestorage.app",
+  messagingSenderId: "575516695545",
+  appId: "1:575516695545:web:b67884b87168151b42fb28",
+  measurementId: "G-5WZ32W3C94"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
