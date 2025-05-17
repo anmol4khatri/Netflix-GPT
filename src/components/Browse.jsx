69eb356ad6b48e216a6513b3e6e842134552f29d
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-05-17 18:36:27 +0530

    feat: implement Header component with logout functionality and clean up Browse component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,26 +1,9 @@
-import { removeUser } from "../utils/userSlice";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
-import { signOut } from "firebase/auth";
-import { auth } from "../utils/firebaseConfig";
+import Header from "./Header";
 
 const Browse = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const handleLogout = () => {
-    signOut(auth).then(() => {
-      // Sign-out successful.
-      dispatch(removeUser());
-      navigate("/");
-    }).catch((error) => {
-      // An error happened.
-    });
-  }
-
   return (
-    <div>
-        <h1>Browse aaygya</h1>
-        <button onClick={handleLogout} className='cursor-pointer bg-amber-400 text-2xl'>Logout</button>
+    <div className="bg-black">
+      <Header />
     </div>
   )
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -114,7 +114,6 @@ const Login = () => {
                     </div>
                 </form>
             </div>
-
         </div>
     );
 };
