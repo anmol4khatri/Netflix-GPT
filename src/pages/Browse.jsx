0edd38c9e06b2c97dc279ed87d6a99f9fb1d45fc
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-05-19 01:26:12 +0530

    feat: implement useNowPlaying hook for fetching now playing movies and add movieSlice for state management

--- src/pages/Browse.jsx
+++ src/pages/Browse.jsx
@@ -1,9 +1,17 @@
 import Header from "../components/Header";
+import useNowPlaying from "../hooks/useNowPlaying";
 
 const Browse = () => {
+
+  // useNowPlaying();
+
   return (
     <div className="bg-black">
       <Header />
+      {/*
+        Poster
+        Discover
+      */}
     </div>
   )
 }
