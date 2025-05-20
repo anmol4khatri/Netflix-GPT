5cba91638bd6f86853769e6f20f4918fedd79176
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-05-20 22:43:15 +0530

    update Header.jsx and VideoText.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,16 +17,16 @@ const Header = () => {
             <div className="top-0 left-0 z-10 from-black flex gap-8">
                 <img className="w-50 max-sm:w-30 max-sm:m-3" src={Netflix_Logo} alt="logo" />
                 <div className="flex gap-8 mt-7 mb-7 text-lg font-semibold text-white">
-                    <h3 className="cursor-pointer">Home</h3>
-                    <h3 className="cursor-pointer">Movies</h3>
-                    <h3 className="cursor-pointer">TV Shows</h3>
-                    <h3 className="cursor-pointer">Latest</h3>
+                    <h3 className="cursor-pointer hover:border-b-2">Home</h3>
+                    <h3 className="cursor-pointer hover:border-b-2">Movies</h3>
+                    <h3 className="cursor-pointer hover:border-b-2">TV Shows</h3>
+                    <h3 className="cursor-pointer hover:border-b-2">Latest</h3>
                 </div>
             </div>
             <div className="m-6 text-white flex gap-5">
                 <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAABb0lEQVR4nO2VO05CQRiFr5hoIVr52AMsAqQ2iqJxCUQLtDGEWCmVj3UYOwkuw4iv1hAW4BMNsVA/M8kh3gYchrmFyT3JVPf7zzlzXxMEsWL9NwGzQBW4At61GsA+MBN1+CrQprdegZUow78VdAZkgAmtLFDTtS9gOYrb3lbATh+uLOYFmPZZoNrduQVbF7vns8C1TDMW7LzYhs8CbzJNWrCTYts+CzwPUGBK7JPPApcyzVqwObEXPgvsyrRmwZ6LrfgsMAc8yrjch6uIeTCfrrcCRubnAnwqoK63PamVC+3cMPkgCgEF/el6yVwrRBW+CNzztwyz4DN4BDgKBdwAJSAVOgvSwBZwK8acGQdm1keBY5l+AEUg0YdNABtijQ6HDc+HwjMDzGVDJZZcw8eBlkyKDvObmm0CYy4F1kPPPOEwPwrcyWPNpcCphksDD/96bMvjxGW4qeHUEAXS3U/TZbhjewJaHM0dV49YsYKo9QOixZ4KLtJpYwAAAABJRU5ErkJggg==" alt="gender-neutral-user"
                     className="cursor-pointer"></img>
-                <h4 className="font-semibold mr-2 mt-1.5 cursor-pointer" onClick={handleLogout}>Logout</h4>
+                <h4 className="font-semibold mr-2 mt-1.5 cursor-pointer hover:border-b-2" onClick={handleLogout}>Logout</h4>
             </div>
         </div>
     );
--- src/components/VideoText.jsx
+++ src/components/VideoText.jsx
@@ -4,12 +4,12 @@ const VideoText = ({ title, overview }) => {
             <div className="w-2/3 pt-80 pl-10">
                 <h1 className="text-5xl mb-7 font-extrabold">{title}</h1>
                 <h3 className="font-semibold">{overview}</h3>
-                <div className="flex gap-4 mt-5 cursor-pointer">
-                    <div className="h-12 flex bg-white text-black p-3 rounded-xl cursor-pointer">
+                <div className="flex gap-6 mt-5 cursor-pointer">
+                    <div className="h-12 flex bg-white text-black p-3 rounded-xl cursor-pointer hover:scale-125">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6 20.1957V3.80421C6 3.01878 6.86395 2.53993 7.53 2.95621L20.6432 11.152C21.2699 11.5436 21.2699 12.4563 20.6432 12.848L7.53 21.0437C6.86395 21.46 6 20.9812 6 20.1957Z"></path></svg>
                         <button className="w-13 cursor-pointer">Play</button>
                     </div>
-                    <div className="h-12 flex bg-[#47484a] p-3 rounded-xl cursor-pointer">
+                    <div className="h-12 flex bg-[#47484a] p-3 rounded-xl cursor-pointer hover:scale-125">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="rgba(255,255,255,1)"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 7H13V9H11V7ZM11 11H13V17H11V11Z"></path></svg>
                         <button className="w-23 cursor-pointer">More Info</button>
                     </div>
